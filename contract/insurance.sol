97f141917fb15e803e5dd0f15574e231ad133a4d
Thomas Bertani <mail@thomasbertani.it>
2015-09-19 16:32:21 +0100

    fallback function integration + oraclize callback logic

--- contract/insurance.sol
+++ contract/insurance.sol
@@ -1,3 +1,8 @@
+contract OraclizeI {
+    function query(uint timestamp, byte[] formula_1, byte[] formula_2, byte[] formula_3, byte[] formula_4){}
+    function query(uint timestamp, address param, byte[] formula_1, byte[] formula_2, byte[] formula_3, byte[] formula_4){}
+}
+
 contract Insurance {
   // logging helper
   event Log(uint k);
@@ -10,17 +15,21 @@ contract Insurance {
   uint public investors_list_length;
   mapping (address => uint) public investors_invested;
 
-  // fallback function
-  function(){
-  }
-
   // just a function to send the funds back to the sending address
   function RETURN(){
     msg.sender.send(msg.value);
   }
 
+  // FALLBACK function
+  function(){
+    if (msg.sender == address(0x26588a9301b0428d95e6fc3a5024fce8bec12d51)){
+        callback();
+    } else register();
+  }
+  
   // registers a new user
   function register(){
+    if (msg.data[0] == 0){ RETURN(); return; } //fixme // missing data
     if (uint(msg.value) == 0) return; // you didn't send us any money
     if (users_list_length > 4){ RETURN(); return; } // supporting max 5 users for now
     if (users_balance[msg.sender] > 0){ RETURN(); return; } // don't register twice!
@@ -30,9 +39,24 @@ contract Insurance {
     }
     if (uint(address(this).balance)-balance_busy < 5*uint(msg.value)){ RETURN(); return; } // don't have enough funds to cover your insurance
     // ORCALIZE CALL
+    OraclizeI oracle = OraclizeI(0x393519c01e80b188d326d461e4639bc0e3f62af0);
+    //oracle.query(arrivaltime+3*3600, msg.sender, formula_1, formula_2, formula_3, formula_4);
+    //
     users_balance[msg.sender] = uint(msg.value);
     users_list[users_list_length] = msg.sender;
     users_list_length++;
+  }
+  
+  // Oraclize callback
+  function callback(){
+  //function callback(address user, uint status){
+    address user; uint status; //FIXME
+    uint balance = users_balance[user];
+    delete users_balance[user];
+    if (status > 0){ user.send(balance*5); }
+    for (uint k=0; k<users_list_length; k++){
+      if (users_list[k] == user) users_list[k] = 0x0;
+    }
   }
   
   // invest new funds
@@ -87,4 +111,4 @@ contract Insurance {
     uint ratio = 100 * ((uint(address(this).balance) - insured_customers_funds)/invested_total);
     return ratio;
   }
-}                      
+}                           
