e4d31d87775469c73f3f691f1164a4864abe81c1
Thomas Bertani <mail@thomasbertani.it>
2015-09-19 13:53:13 +0100

    register method is now there

--- contract/insurance.sol
+++ contract/insurance.sol
@@ -12,6 +12,13 @@ contract Insurance {
   
   // registers a new user
   function register(){
+    if (uint(msg.value) == 0) stop(); // you didn't send us any money
+    if (users_length > 4) stop(); // supporting max 5 users for now
+    if (users_balance[msg.sender]) stop(); // don't register twice!
+    uint balance_busy = 0; // TODO: calculate actual balance_busy value
+    if (uint(address(this).balance)-balance_busy < 5*uint(msg.value)) stop(); // don't have enough funds to cover your insurance
+    // ORCALIZE CALL
+    users_balance[msg.sender] = uint(msg.value);
     users_list[users_length] = msg.sender;
     users_length++;
   }
@@ -24,5 +31,9 @@ contract Insurance {
   // get a specific user invested amount
   function get_user(address user) returns (uint){
     return users_balance[user];
+  }
+  
+  // returns percentage performance data about this investment
+  function investment_ratio() returns (uint){
   }
 }
